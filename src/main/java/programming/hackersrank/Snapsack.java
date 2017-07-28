3c8bbab10a24df9185f36d349c3912a75a0e2c7f
cgauna <cgauna@despegar.com>
2017-07-28 08:59:43 -0300

    Finish coin change

---
+++ src/main/java/programming/hackersrank/Snapsack.java
@@ -0,0 +1,11 @@
+package programming.hackersrank;
+
+import java.util.Scanner;
+
+/** Created by calasius on 7/26/17. */
+public class Snapsack {
+
+  public static void main(String... args) {
+    Scanner scanner = new Scanner(System.in);
+  }
+}
